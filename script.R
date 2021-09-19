4d9f382971093cf2a27924ebca36c603383f9042
Jared Whalen <jared.m.whalen@gmail.com>
2021-09-19 18:37:54 -0400

    Update script.R

--- script.R
+++ script.R
@@ -21,7 +21,7 @@ updateCaseData = function() {
 
 checkForHospitalizationUpdate = function() {
   source("getHospitalizations.R")
-  hospitalizations__old <- read_csv(paste("https://www.gannett-cdn.com/delaware-online/datasets/coronavirus-tracker/latest/hospitalizations.csv", stringi::stri_rand_strings(1, 50), sep='') %>%
+  hospitalizations__old <- read_csv(paste("https://www.gannett-cdn.com/delaware-online/datasets/coronavirus-tracker/latest/hospitalizations.csv", stringi::stri_rand_strings(1, 50), sep='')) %>%
     select(date_confirmed, delaware_hospitalized) %>% 
     mutate(date_confirmed = as.Date(date_confirmed), delaware_hospitalized = as.numeric(delaware_hospitalized)) %>% 
     arrange(date_confirmed)
